1ad604cfb5687a161f303290b623784e7cf3fc9d
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-05-03 15:20:47 +0530

    signup routering

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthServiceService } from '../auth-service.service';
+import { FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  SigninForm!: FormGroup
+
+  constructor(private authservice: AuthServiceService,
+              private router: Router) {
+
+  }
+  ngOnInit(): void {
+    this.SigninForm=new FormGroup({
+      email:new FormGroup('',Validators.required),
+      password:new FormGroup('',Validators.required)
+    })
+  }
+
+  signIn() {
+    this.router.navigate(['./home']);
+  }
 
 }
