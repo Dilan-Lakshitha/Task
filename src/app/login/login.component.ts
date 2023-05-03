c43e75d4c2f4828ce1027a348b7e2e9386676900
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-05-03 10:27:42 +0530

    login interface with click button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private router: Router) {
 
+  }
+
+  loginClick() {
+    this.router.navigate(['./home']);
+  }
+
+  signupClick(){
+    this.router.navigate(['./sign-up']);
+  }
 }
