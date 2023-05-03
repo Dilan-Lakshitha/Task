990cba99579710377785024b877245f90bdc6ef1
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-05-03 11:03:25 +0530

    given validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthServiceService } from '../auth-service.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  constructor(private router: Router) {
+export class LoginComponent implements OnInit {
+  loginForm!:FormGroup;
+
+  constructor(private authservice:AuthServiceService, 
+              private router:Router) {
 
   }
-
-  loginClick() {
-    this.router.navigate(['./home']);
+  ngOnInit(): void {
+    this.loginForm =new FormGroup({
+      email:new FormGroup('',[Validators.required]),
+      password:new FormGroup('',[Validators.required])
+    })
   }
+
+  // loginClick() {
+  //   this.router.navigate(['./home']);
+  // }
 
   signupClick(){
     this.router.navigate(['./sign-up']);
+  }
+  loginProcess(){
+    this.router.navigate(['./home']);
+
+    if(this.loginForm.valid){
+      this.authservice.login(this.loginForm.value).subscribe(result=>{
+        if(result.success){
+          console.log(result);
+          alert(result.message);
+        }
+      })
+    }
   }
 }
