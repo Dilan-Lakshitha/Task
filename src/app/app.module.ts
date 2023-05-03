fe53e584e7a81e15e263d647496c46f62d81aa81
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-05-03 10:27:22 +0530

    create a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule} from "@angular/common/http"
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'',component:LoginComponent},
+      {path:'home',component:HomeComponent},
+      {path:'sign-up',component:SignUpComponent}
+  ]),
+  HttpClientModule,
+  FormsModule,
+  ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
